826bbc6e3fa3b413c1a44411612169a8d2f506dc
Herbert-Haase <herberthaase02@gmx.de>
2025-10-29 14:51:53 +0100

    wrote wip from laptop

--- src/main/scala/TUI.scala
+++ src/main/scala/TUI.scala
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package de.htwg.webcrawler {
   object TUI
   def main(args: Array[String]): Unit =
@@ -15,4 +16,47 @@ package de.htwg.webcrawler {
     println(horizontal_line)
     // development
     // development2
+=======
+import scala.io.Source
+import java.io.File
+
+@main def tui(s: Int): Unit = {
+  val filename = "./TUI.scala"
+  print_window(s, filename)
+}
+
+def print_fromFile(line_width: Int, filename: File): String = {
+  val leftover_w = False
+  var result = ""
+  for (line <- Source.fromFile(filename).getLines) {
+    val words = line.split("\\s+")
+    // val line_str = words.mkString(" ")
+    for (word <- words) {
+      if (line_width <= words.len) {
+        line_width -= words.len
+        // concat word to line
+      } else {
+        leftover_w = True
+        // concat \n to line
+      }
+    }
+    if (leftover_w) {
+      return text_line
+    } else {
+      return text_line + "\n"
+    }
+  }
+}
+
+def print_window(s: Int, file: File): Unit = {
+  val width = s * (s - 1)
+  val horizontal_line = "+" + "-" * width + "+"
+
+  println(horizontal_line)
+
+  for (i <- 0 until s * 3)
+    println("|" + print_fromFile(width, file) + "|")
+
+  println(horizontal_line)
+>>>>>>> Stashed changes
 }
